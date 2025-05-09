57c76993da724d94ad96cf618f827ea82861fd7a
Patrick Knueppel <pknueppel@comcast.net>
2025-05-09 14:00:15 -0400

    Initial configuration

---
+++ waybar/scripts/fetch.sh
@@ -0,0 +1,8 @@
+# super super cool neofetch script!!!111 people will love that I use arch!!!
+
+
+#! /usr/bin/bash
+
+fastfetch
+
+read -p "Press [Enter] to close"
---
+++ waybar/scripts/update.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+printf "Updating system:\n\n" 
+
+yay -Syu
+
+printf "\n\nUpdate finished.\n\n"
+
+read -p "Press [Enter] to close"
